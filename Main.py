f31de32269fc63d008d043a442bc27d13a140919
Amir Mohammad Karimi <35371206+AMK9978@users.noreply.github.com>
2018-12-19 20:02:55 +0330

    Add files via upload

--- Main.py
+++ Main.py
@@ -4,70 +4,60 @@ import os
 import queue
 
 
-# from anytree import Node, RenderTree
+class Queue_Object:
+    def __init__(self, link, node, path):
+        self.link = link
+        self.path = path
+        self.node = node
 
 
 def calc(link, counter, pre_path, pre_node):
-    if counter == given_level - 1:
-        return
-    counter += 1
-    # try:
-    r = requests.get(link)
-    content = r.text
-    key = '<a href=[\'\"](.*?)[\'\"]'
-    List = list(set(re.findall(key, content)))
-    j = 1
-    for i in range(len(List)):
-        if len(List[i]) > 2:
-            print('mehrnoush:%s,%s,,,%s' % (List[i][0], List[i][1], List[i][0]+List[i][1]))
-            if List[i][0] + List[i][1] != 'ht':
-                List[i] = link + List[i]
-        else:
-            List[i] = link + List[i]
-        node = pre_node + "." + str(j)
-        path = pre_path + "/" + node
-        # t = Node(node, parent=pre_node)
-        d[path] = List[i]
-        os.mkdir(path)
-        calc(List[i], counter, path, node)
-        j += 1
-    print("my List: %s" %str(List))
-
-    q.put(List)
-    print(q.get())
-
-
-
-def search(link):
-    for key, value in d.items():
-        if value == link:
-            print("Yes!")
-            print(key)
+    try:
+        q = queue.Queue()
+        if counter == given_level - 1:
             return
+        counter += 1
+        # try:
+        r = requests.get(link)
+        content = r.text
+        key = '<a href=[\'\"](.*?)[\'\"]'
+        List = list(set(re.findall(key, content)))
+        j = 1
+        for i in range(len(List)):
+            # To avoid repetitive values in dict:
+            try:
+                if List[i] in d.values():
+                    continue
+            except Exception as e:
+                print(str(i))
+            # To save accurate links:
+            if len(List[i]) > 2:
+                # print('HAHA:%s,%s,,,%s' % (List[i][0], List[i][1], List[i][0] + List[i][1]))
+                if List[i][0] + List[i][1] != 'ht':
+                    List[i] = link + List[i]
+            else:
+                List[i] = link + List[i]
+            node = pre_node + "." + str(j)
+            path = pre_path + "/" + node
+            Q = Queue_Object(List[i], node, path)
+            # make directory
+            q.put(Q)
+            d[path] = List[i]
+            os.mkdir(path)
+            j += 1
+        print("my List: %s" % str(List))
+        while q.qsize() != 0:
+            Q = q.get()
+            calc(Q.link, counter, Q.path, Q.node)
+    except Exception as e:
+        print(str(e)+","+link)
 
-    print("No!")
-    return
-
-
-# def main():
-q = queue.Queue()
 d = {}  # hash table
 
 counter = 0
 given_link = input('please input your line:').strip()
 given_level = int(input('please enter a level: ').strip())
 
-# t = Node("C")  # tree
-# subT = []
-# subT = Node("1", parent="C")
 d["C:/1"] = given_link
 os.mkdir("C:/1")
 calc(given_link, 0, "C:/1", "1")
-# for i in d:
-#     print(i)
-search(given_link)
-# for pre, fill, node in RenderTree(t):
-#     print("%s%s" % (pre, node.name))
-
-# if __name__ == 'main':
-#     main()
