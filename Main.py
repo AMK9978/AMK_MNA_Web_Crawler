b472db76c40d1198708fc1e35e3007d374e68ca6
Mehrnoush504 <44124890+Mehrnoush504@users.noreply.github.com>
2018-12-19 18:08:09 +0330

    Add files via upload
    added queue, dictionary,  creating directory

--- Main.py
+++ Main.py
@@ -1,33 +1,73 @@
 import re
 import requests
+import os
+import queue
 
-def calc(link,counter):
-    if counter == 4:
+
+# from anytree import Node, RenderTree
+
+
+def calc(link, counter, pre_path, pre_node):
+    if counter == given_level - 1:
         return
     counter += 1
-    try:
-        r = requests.get(link)
-        content = r.text
-        key = '<a href=[\'\"](.*?)[\'\"]'
-        list = set(re.findall(key,content))
-        for i in list:
-            print('link is:' ,end='')
-            if len(i) <2 or i[0]+i[1] != 'ht':
-                i = link + i
-                print(i)
-            else:
-                print(i)
-            calc(i,counter)
-    except Exception as ex:
-        print(str(ex))
+    # try:
+    r = requests.get(link)
+    content = r.text
+    key = '<a href=[\'\"](.*?)[\'\"]'
+    List = list(set(re.findall(key, content)))
+    j = 1
+    for i in range(len(List)):
+        if len(List[i]) > 2:
+            print('mehrnoush:%s,%s,,,%s' % (List[i][0], List[i][1], List[i][0]+List[i][1]))
+            if List[i][0] + List[i][1] != 'ht':
+                List[i] = link + List[i]
+        else:
+            List[i] = link + List[i]
+        node = pre_node + "." + str(j)
+        path = pre_path + "/" + node
+        # t = Node(node, parent=pre_node)
+        d[path] = List[i]
+        os.mkdir(path)
+        calc(List[i], counter, path, node)
+        j += 1
+    print("my List: %s" %str(List))
+
+    q.put(List)
+    print(q.get())
+
+
+
+def search(link):
+    for key, value in d.items():
+        if value == link:
+            print("Yes!")
+            print(key)
+            return
+
+    print("No!")
+    return
+
+
 # def main():
-list = ()
+q = queue.Queue()
+d = {}  # hash table
+
 counter = 0
-link = input('please input your line:').strip()
-calc(link,0)
+given_link = input('please input your line:').strip()
+given_level = int(input('please enter a level: ').strip())
+
+# t = Node("C")  # tree
+# subT = []
+# subT = Node("1", parent="C")
+d["C:/1"] = given_link
+os.mkdir("C:/1")
+calc(given_link, 0, "C:/1", "1")
+# for i in d:
+#     print(i)
+search(given_link)
+# for pre, fill, node in RenderTree(t):
+#     print("%s%s" % (pre, node.name))
 
 # if __name__ == 'main':
 #     main()
-
-
-
